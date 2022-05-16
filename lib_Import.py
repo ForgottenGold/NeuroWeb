39e9b0d1a7d45f58e5c088c7654e96f56ce21411
zabitoezoloto <zabitoezoloto@gmail.com>
2022-05-16 19:54:44 +0500

    Рефакторинг

--- lib_Import.py
+++ lib_Import.py
@@ -1,12 +1,12 @@
 print("Импорт библиотек...")
 
 import matplotlib; matplotlib.use("Agg")
+import matplotlib.pyplot as mpl
 import tensorflow as tf
+import random as rand
+import pickle as pic
 import numpy as np
-import matplotlib.pyplot as plt
 import pylab as pl
-import random
-import pickle
 import cv2
 import os
 
@@ -14,8 +14,7 @@ from keras.optimizer_v2.gradient_descent import SGD
 from keras.optimizer_v2.adam import Adam
 from keras.losses import MeanSquaredError
 from keras.models import Sequential
-from keras.layers import Input
-from keras.layers import Dense, Conv1D, Conv2D, MaxPool1D, Dropout, Activation
+from keras.layers import Input, Dense, Conv1D, Conv2D, MaxPool1D, Dropout, Activation
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
